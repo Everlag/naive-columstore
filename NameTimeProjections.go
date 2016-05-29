ff13dff0fe534ec8e15cec8c88578dd309ae643d
Everlag <kyleveb@gmail.com>
2016-05-29 16:04:30 -0600

    introduce NameTimeProjection and test for ordering sanity
    NameTimeProjection is a great first projection as Names should encode to
    extremely sparse representations while Times should be able to be delta
    encoded.

---
+++ NameTimeProjections.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"sort"
+	"time"
+)
+
+// Define a projection sorted first by name
+// then by time
+type NameTimeProjection struct {
+	Names FiniteString32Column
+	Sets  FiniteString32Column
+
+	Prices UInt32Column
+
+	Times TimeColumn
+}
+
+// Generate a NameTimeProjection from a fully
+// filled PriceDB.
+//
+// I'm not handling updates so this is fine... in theory.
+func NameTimeProjectionFromPriceDB(db PriceDB) NameTimeProjection {
+	proj := NameTimeProjection{
+		Names:  NewFiniteString32Column(),
+		Sets:   NewFiniteString32Column(),
+		Prices: NewUInt32Column(),
+		Times:  NewTimeColumn(),
+	}
+
+	// Determine full length of database
+	length := db.Prices.Length()
+
+	// Fetch fully materialized tuples
+	//
+	// TODO: avoid full materialization of the entire
+	// freaking dataset...
+	tuples := make([]PriceTuple, length)
+	for i := 0; i < length; i++ {
+		tuple := PriceTuple{
+			Name:  db.Names.Access(i),
+			Set:   db.Sets.Access(i),
+			Price: db.Prices.Access(i),
+			Time:  db.Times.Access(i),
+		}
+
+		tuples[i] = tuple
+	}
+
+	// Sort the tuples according to Name then time
+	sort.Sort(NameTimeOrderedTuples(tuples))
+
+	proj.Push(tuples)
+
+	return proj
+}
+
+func (proj *NameTimeProjection) Push(values []PriceTuple) {
+	names := make([]string, len(values))
+	sets := make([]string, len(values))
+	prices := make([]uint32, len(values))
+	times := make([]time.Time, len(values))
+	for i, p := range values {
+		names[i] = p.Name
+		sets[i] = p.Set
+		prices[i] = p.Price
+		times[i] = p.Time
+	}
+	proj.Names.Push(names)
+	proj.Sets.Push(sets)
+	proj.Prices.Push(prices)
+	proj.Times.Push(times)
+}
+
+type NameTimeOrderedTuples []PriceTuple
+
+func (a NameTimeOrderedTuples) Len() int {
+	return len(a)
+}
+func (a NameTimeOrderedTuples) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+func (a NameTimeOrderedTuples) Less(i, j int) bool {
+	first := a[i]
+	second := a[j]
+
+	// If names are same, fall back to time based
+	if first.Name == second.Name {
+		return first.Time.Before(second.Time)
+	}
+
+	return first.Name < second.Name
+}
---
+++ NameTimeProjections_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"testing"
+
+	"time"
+)
+
+var TestNameTimeProjection *NameTimeProjection
+
+// Create the testing projection we use
+//
+// This allows tests to run faster as anything using this projection
+// can use the same copy
+func setupNameTimeProjectionTest(t *testing.T) NameTimeProjection {
+
+	// Make sure the underlying db was created first
+	db := setupPriceTest(t)
+	proj := NameTimeProjectionFromPriceDB(db)
+
+	TestNameTimeProjection = &proj
+
+	return proj
+}
+
+// Create a projection out of the fully formed db
+//
+// Postgres equivalent
+//  n/a
+func TestNameTimeProjectionFromPriceDB(t *testing.T) {
+	// Ensure test DB and projection are setup
+	//
+	// Not strictly necessary but we use TestDB anyway
+	proj := setupNameTimeProjectionTest(t)
+	db := setupPriceTest(t)
+
+	// Ensure price length is equal
+	priceLengthDB := db.Prices.Length()
+	priceLengthProj := proj.Prices.Length()
+	if priceLengthDB != priceLengthProj {
+		t.Fatalf("mismatching projection and db lengths %v != %v", priceLengthDB, priceLengthProj)
+	}
+
+	// Ensure sort was successful in making time ascending
+	badTransitions := 0
+
+	var prevName string
+	var prevTime time.Time
+	for i := 0; i < priceLengthProj; i++ {
+		name := proj.Names.Access(i)
+		time := proj.Times.Access(i)
+
+		// Unequal names should have previous time AFTER current time
+		if name != prevName && prevTime.Before(time) {
+			// Dataset can be a little dirty due to unhinged and such
+			// so we keep track of how many times this happeened
+			// and threshold the test failure
+			badTransitions++
+			if badTransitions > 5 {
+				t.Fatalf("not asc sort order for index %v: {%v, %v} {%v, %v}",
+					i, prevName, prevTime, name, time)
+			}
+		}
+
+		// Equal names should have previous time BEFORE current time
+		if name == prevName && prevTime.After(time) {
+			t.Fatalf("not asc sort order for index %v: {%v, %v} {%v, %v}",
+				i, prevName, prevTime, name, time)
+		}
+
+		prevName = name
+		prevTime = time
+	}
+
+}
