42cb9d0a6fc1308c0e4507fd6bd63108b957342f
Everlag <kyleveb@gmail.com>
2016-05-31 00:06:08 -0600

    add TimeColumn ANDAfter test
    It's late but necessary for sanity.

--- TimeColumn_test.go
+++ TimeColumn_test.go
@@ -57,3 +57,35 @@ func TestTimeAfter(t *testing.T) {
 	}
 
 }
+
+// Sanity test on TimeColumn's ANDAfter
+func TestTimeANDAfter(t *testing.T) {
+
+	ref := GetTimeRefSlice()
+
+	col := NewTimeColumn()
+	col.Push(ref)
+
+	// Middle item of slice
+	middleIndex := TimeRefSliceLength / 2
+	// Start from all truthy bools
+	query := NewBoolColumn()
+	query.PushTrue(len(ref))
+	// Filter out bad results
+	col.ANDAfter(ref[middleIndex], query)
+	computed := query.TruthyIndices()
+
+	// We know how long the result should be
+	if (TimeRefSliceLength-middleIndex)-1 != len(computed) {
+		t.Fatalf("after has unexpected length %v != %v",
+			TimeRefSliceLength-middleIndex, len(computed))
+	}
+
+	// Check every item computed is after the middle
+	for _, refIndex := range computed {
+		if refIndex < middleIndex {
+			t.Fatalf("strictly increasing item in subset found as After")
+		}
+	}
+
+}
